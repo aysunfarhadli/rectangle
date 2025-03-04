1d666c3201b0bf0a52463403fa8acc95c526e4e0
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 13:11:10 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Düzbucaqlının enini daxil edin: ");
+        Console.Write("Düzbucaqlının enini daxil edin");
         double en = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Düzbucaqlının uzunluğunu daxil edin: ");
+        Console.Write("Düzbucaqlının uzunluğunu daxil edin");
         double uzunluq = Convert.ToDouble(Console.ReadLine());
 
         double sahe = en * uzunluq;
         double perimetr = 2 * (en + uzunluq);
 
-        Console.WriteLine("Düzbucaqlının sahəsi: " + sahe);
-        Console.WriteLine("Düzbucaqlının perimetri: " + perimetr);
+        Console.WriteLine(sahe);
+        Console.WriteLine(perimetr);
     }
 }
