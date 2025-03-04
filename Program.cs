44fcd10c5dc73e82ee96edfee04dd2ffa466d434
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 12:01:29 -0800

    code done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Düzbucaqlının enini daxil edin: ");
+        double en = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Düzbucaqlının uzunluğunu daxil edin: ");
+        double uzunluq = Convert.ToDouble(Console.ReadLine());
+
+        double sahe = en * uzunluq;
+        double perimetr = 2 * (en + uzunluq);
+
+        Console.WriteLine("Düzbucaqlının sahəsi: " + sahe);
+        Console.WriteLine("Düzbucaqlının perimetri: " + perimetr);
+    }
+}
